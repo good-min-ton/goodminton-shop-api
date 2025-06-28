4225bbb9a53da513e16a57e5f31d70583747f0aa
Lezh1n <hoangphilong1208@gmail.com>
2025-06-28 20:49:00 +0700

    login dtos

--- src/main/java/com/lezh1n/goodminton_shop_api/dto/request/LoginRequest.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/dto/request/LoginRequest.java
@@ -1,5 +1,9 @@
 package com.lezh1n.goodminton_shop_api.dto.request;
 
+import lombok.Getter;
+
+@Getter
 public class LoginRequest {
-    
+    private String identifier;
+    private String password;
 }
--- src/main/java/com/lezh1n/goodminton_shop_api/dto/response/AuthenticationResponse.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/dto/response/AuthenticationResponse.java
@@ -6,5 +6,6 @@ import lombok.Data;
 @Data
 @Builder
 public class AuthenticationResponse {
-
+    private String accessToken;
+    private String refreshToken;
 }
