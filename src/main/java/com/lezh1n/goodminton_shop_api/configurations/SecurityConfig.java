f3de54d20ed885343a195a57b07a44aed169c236
Lezh1n <hoangphilong1208@gmail.com>
2025-06-28 15:57:58 +0700

    config security

--- src/main/java/com/lezh1n/goodminton_shop_api/configurations/SecurityConfig.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/configurations/SecurityConfig.java
@@ -1,11 +1,21 @@
 package com.lezh1n.goodminton_shop_api.configurations;
 
+import java.util.Collections;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -21,6 +31,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SecurityConfig {
 
+    private final JwtBlacklistFilter jwtBlacklistFilter;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.secret-key}")
+    private String jwtSecret;
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -33,7 +48,7 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-    
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -49,5 +64,26 @@ public class SecurityConfig {
                                 .decoder(jwtDecoder())
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter())));
         return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withSecretKey(
+                new SecretKeySpec(jwtSecret.getBytes(), "HS512"))
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            String role = jwt.getClaim("role");
+            log.info("JWT role: " + role);
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
+            log.info("Created authority: " + authority);
+            return role != null ? Collections.singletonList(authority) : Collections.emptyList();
+        });
+        return converter;
     }
 }
