93b74fa18da3ad3d33be9f087d986cba86c48509
Lezh1n <hoangphilong1208@gmail.com>
2025-06-29 22:54:53 +0700

    refactor

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE account (
     account_id SERIAL PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     phone VARCHAR(20) UNIQUE,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role user_role NOT NULL,
     status account_status NOT NULL
