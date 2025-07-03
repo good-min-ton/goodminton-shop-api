5b11e06f71af0c185184e274429195c7aa83afb3
Lezh1n <hoangphilong1208@gmail.com>
2025-07-03 09:50:29 +0700

    feat: create store admin's account

--- src/main/java/com/lezh1n/goodminton_shop_api/controllers/AccountController.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/controllers/AccountController.java
@@ -3,13 +3,18 @@ package com.lezh1n.goodminton_shop_api.controllers;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lezh1n.goodminton_shop_api.dtos.ApiResponse;
+import com.lezh1n.goodminton_shop_api.dtos.request.CreateAccountRequest;
 import com.lezh1n.goodminton_shop_api.dtos.response.AccountResponse;
+import com.lezh1n.goodminton_shop_api.enums.UserRole;
 import com.lezh1n.goodminton_shop_api.services.AccountService;
+import com.lezh1n.goodminton_shop_api.services.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class AccountController {
 
     private final AccountService accountService;
+    private final AuthService authService;
 
     @GetMapping("/{id}")
     public ApiResponse<AccountResponse> getAccountById(@PathVariable Integer id) {
@@ -37,6 +43,13 @@ public class AccountController {
         Page<AccountResponse> accountPage = accountService.getAllAccounts(page, size, sortBy, sortDir);
         return ApiResponse.<Page<AccountResponse>>builder()
                 .result(accountPage)
+                .build();
+    }
+
+    @PostMapping("/store-admin")
+    public ApiResponse<AccountResponse> register(@RequestBody CreateAccountRequest request) {
+        return ApiResponse.<AccountResponse>builder()
+                .result(authService.register(request, UserRole.STORE_ADMIN))
                 .build();
     }
 }
