d061f9e8195ac5c914940c6475305fef067367f9
Lezh1n <hoangphilong1208@gmail.com>
2025-07-03 12:21:35 +0700

    config pagination

---
+++ src/main/java/com/lezh1n/goodminton_shop_api/configurations/JacksonConfig.java
@@ -0,0 +1,9 @@
+package com.lezh1n.goodminton_shop_api.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class JacksonConfig {
+}
