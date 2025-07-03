23fe9a9e9e2651f4788170c3f2e7b861513bfe52
Lezh1n <hoangphilong1208@gmail.com>
2025-07-03 10:20:02 +0700

    chore

--- src/main/resources/db/migration/V2__init_super_admin.sql
+++ src/main/resources/db/migration/V2__init_super_admin.sql
@@ -1,12 +1,17 @@
 -- Create super admin account
-INSERT INTO
-    account (full_name, phone, email, password, role, status)
-VALUES
-    (
-        'Super Admin',
-        '0123456789',
-        'goodmintonadm@gmail.com',
-        '$2a$10$thPE6RvZTakNYb9jNTAhTeBpFFITPXWOfoYwHCzrcMVQrQbS906iC', -- bcrypt: Admin@123
-        'SUPER_ADMIN',
-        'ACTIVE'
-    );
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM account WHERE email = 'admin@goodminton.com'
+    ) THEN
+        INSERT INTO account (full_name, phone, email, password, role, status)
+        VALUES (
+            'Super Admin',
+            '0999999999',
+            'admin@goodminton.com',
+            '$2a$10$5bCEzE7L.LcrZp/CcnFY5Ov7LkP9Qpi8nWB7z8DqINh2tYOG4cKu6', -- bcrypt: Admin@123
+            'SUPER_ADMIN',
+            'ACTIVE'
+        );
+    END IF;
+END $$;
