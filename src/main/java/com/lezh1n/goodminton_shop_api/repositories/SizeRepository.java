b7e29782de0b8feda198bcb5500988e73ee96a03
Lezh1n <hoangphilong1208@gmail.com>
2025-07-08 19:27:49 +0700

    create repositories

---
+++ src/main/java/com/lezh1n/goodminton_shop_api/repositories/SizeRepository.java
@@ -0,0 +1,11 @@
+package com.lezh1n.goodminton_shop_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lezh1n.goodminton_shop_api.entities.Size;
+
+@Repository
+public interface SizeRepository extends JpaRepository<Size, Integer> {
+
+}
