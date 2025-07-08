ca4005155e2f450374f961107372e187f4671986
Lezh1n <hoangphilong1208@gmail.com>
2025-07-08 11:23:36 +0700

    category controller

---
+++ src/main/java/com/lezh1n/goodminton_shop_api/controllers/CategoryController.java
@@ -0,0 +1,57 @@
+package com.lezh1n.goodminton_shop_api.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lezh1n.goodminton_shop_api.dtos.ApiResponse;
+import com.lezh1n.goodminton_shop_api.dtos.request.CategoryRequest;
+import com.lezh1n.goodminton_shop_api.dtos.response.CategoryResponse;
+import com.lezh1n.goodminton_shop_api.services.CategoryService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ApiResponse<CategoryResponse> createCategory(@Valid @RequestBody CategoryRequest request) {
+        return ApiResponse.<CategoryResponse>builder()
+                .result(categoryService.createCategory(request))
+                .build();
+    }
+
+    @GetMapping("/{categoryId}")
+    public ApiResponse<CategoryResponse> getCategoryById(@PathVariable Integer categoryId) {
+        return ApiResponse.<CategoryResponse>builder()
+                .result(categoryService.getCategoryById(categoryId))
+                .build();
+    }
+
+    @GetMapping
+    public ApiResponse<List<CategoryResponse>> getAllCategories() {
+        return ApiResponse.<List<CategoryResponse>>builder()
+                .result(categoryService.getAllCategories())
+                .build();
+    }
+
+    @PutMapping("/{categoryId}")
+    public ApiResponse<CategoryResponse> updateCategory(
+            @PathVariable Integer categoryId,
+            @Valid @RequestBody CategoryRequest request) {
+        return ApiResponse.<CategoryResponse>builder()
+                .result(categoryService.updateCategory(categoryId, request))
+                .build();
+    }
+}
