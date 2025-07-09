0d00819e256b9db2684e883e952f510b9e76bdae
Lezh1n <hoangphilong1208@gmail.com>
2025-07-09 19:25:41 +0700

    refactor

--- src/main/java/com/lezh1n/goodminton_shop_api/repositories/InventoryRepository.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/repositories/InventoryRepository.java
@@ -1,11 +1,14 @@
 package com.lezh1n.goodminton_shop_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.lezh1n.goodminton_shop_api.entities.Inventory;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
-    boolean existsByStoreId(Integer storeId);
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM inventory WHERE store_id = :storeId)", nativeQuery = true)
+    boolean existsByStoreId(@Param("storeId") Integer storeId);
 }
