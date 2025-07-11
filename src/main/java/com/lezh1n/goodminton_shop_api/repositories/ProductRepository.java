b968fce286faa7826145a0040a7dc6eac2bdc988
Lezh1n <hoangphilong1208@gmail.com>
2025-07-11 18:21:32 +0700

    refactor

--- src/main/java/com/lezh1n/goodminton_shop_api/repositories/ProductRepository.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/repositories/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.lezh1n.goodminton_shop_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.lezh1n.goodminton_shop_api.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-    boolean existsByCategoryId(Integer categoryId);
+    
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM product WHERE category_id = :categoryId)", nativeQuery = true)
+    boolean existsByCategoryId(@Param("categoryId") Integer categoryId);
 }
