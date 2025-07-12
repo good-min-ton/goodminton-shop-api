2a02f991157566e75e86936b612a54513fb9f8c0
Lezh1n <hoangphilong1208@gmail.com>
2025-07-12 15:34:03 +0700

    refactor

--- src/main/java/com/lezh1n/goodminton_shop_api/dtos/request/CreateStoreRequest.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/dtos/request/CreateStoreRequest.java
@@ -3,6 +3,7 @@ package com.lezh1n.goodminton_shop_api.dtos.request;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
@@ -19,12 +20,12 @@ public class CreateStoreRequest {
     @Size(min = 10, max = 12, message = "AUTH_INVALID_PHONE")
     private String contact;
 
-    @NotBlank(message = "STORE_ADDRESS_BLANK")
+    @NotNull(message = "STORE_ADDRESS_BLANK")
     private BigDecimal longitude;
 
-    @NotBlank(message = "STORE_ADDRESS_BLANK")
+    @NotNull(message = "STORE_ADDRESS_BLANK")
     private BigDecimal latitude;
 
-    @NotBlank(message = "STORE_ADMIN_BLANK")
+    @NotNull(message = "STORE_ADMIN_BLANK")
     private Integer adminId;
 }
