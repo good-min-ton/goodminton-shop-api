7824d26400570e3c754b71bef39f43d655082e8f
Lezh1n <hoangphilong1208@gmail.com>
2025-07-23 20:58:16 +0700

    refactor entities to use entity graph

--- src/main/java/com/lezh1n/goodminton_shop_api/entities/Product.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/entities/Product.java
@@ -1,14 +1,19 @@
 package com.lezh1n.goodminton_shop_api.entities;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -46,4 +51,12 @@ public class Product {
 
     @Column(name = "create_at", nullable = false)
     private LocalDateTime createAt;
+
+    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<ProductVariant> variants = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<ProductSpecification> specifications = new ArrayList<>();
 }
--- src/main/java/com/lezh1n/goodminton_shop_api/entities/ProductVariant.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/entities/ProductVariant.java
@@ -1,12 +1,18 @@
 package com.lezh1n.goodminton_shop_api.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,4 +43,12 @@ public class ProductVariant {
     @ManyToOne
     @JoinColumn(name = "color_id")
     private Color color;
+
+    @OneToMany(mappedBy = "productVariant", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<VariantSize> sizes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "variant", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<VariantImage> images = new ArrayList<>();
 }
