6ec7c19fbbc9ecee025f19ce3843222111e78281
Lezh1n <hoangphilong1208@gmail.com>
2025-07-24 16:42:21 +0700

    add to entity methods

--- src/main/java/com/lezh1n/goodminton_shop_api/mappers/VariantSizeMapper.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/mappers/VariantSizeMapper.java
@@ -4,8 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Component;
 
+import com.lezh1n.goodminton_shop_api.dtos.request.VariantSizeRequest;
 import com.lezh1n.goodminton_shop_api.dtos.response.VariantSizeResponse;
+import com.lezh1n.goodminton_shop_api.entities.ProductVariant;
+import com.lezh1n.goodminton_shop_api.entities.Size;
 import com.lezh1n.goodminton_shop_api.entities.VariantSize;
+import com.lezh1n.goodminton_shop_api.exceptions.AppException;
+import com.lezh1n.goodminton_shop_api.exceptions.ErrorCode;
+import com.lezh1n.goodminton_shop_api.repositories.SizeRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,7 +19,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class VariantSizeMapper {
 
+    private final SizeRepository sizeRepository;
     private final SizeMapper sizeMapper;
+
+    public VariantSize toVariantSize(ProductVariant variant, VariantSizeRequest request) {
+        Size size = sizeRepository.findById(request.getSizeId())
+                .orElseThrow(() -> new AppException(ErrorCode.SIZE_NOT_FOUND));
+        return VariantSize.builder()
+                .variant(variant)
+                .size(size)
+                .price(request.getPrice())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
 
     public VariantSizeResponse toVariantSizeResponse(VariantSize variantSize) {
         return VariantSizeResponse.builder()
