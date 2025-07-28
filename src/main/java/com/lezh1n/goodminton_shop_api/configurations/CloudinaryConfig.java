c3df962b8d24bfe7b29ae36f5dce01e97649de90
Lezh1n <hoangphilong1208@gmail.com>
2025-07-28 22:13:08 +0700

    config cloudinary

--- src/main/java/com/lezh1n/goodminton_shop_api/configurations/CloudinaryConfig.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/configurations/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package com.lezh1n.goodminton_shop_api.configurations;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.cloudinary.Cloudinary;
@@ -9,15 +10,16 @@ import com.cloudinary.utils.ObjectUtils;
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud_name}")
+    @Value("${cloudinary.cloud-name}")
     private String cloudName;
 
-    @Value("${cloudinary.api_key}")
+    @Value("${cloudinary.api-key}")
     private String apiKey;
 
-    @Value("${cloudinary.api_secret}")
+    @Value("${cloudinary.api-secret}")
     private String apiSecret;
 
+    @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", cloudName,
