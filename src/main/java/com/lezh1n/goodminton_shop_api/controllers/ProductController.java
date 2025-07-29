4418db44200fcb015138b8238eb18ecc399b2658
Lezh1n <hoangphilong1208@gmail.com>
2025-07-29 23:24:08 +0700

    complete product controller

--- src/main/java/com/lezh1n/goodminton_shop_api/controllers/ProductController.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/controllers/ProductController.java
@@ -1,9 +1,11 @@
 package com.lezh1n.goodminton_shop_api.controllers;
 
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lezh1n.goodminton_shop_api.dtos.ApiResponse;
 import com.lezh1n.goodminton_shop_api.dtos.request.ProductRequest;
+import com.lezh1n.goodminton_shop_api.dtos.request.ProductSpecificationRequest;
+import com.lezh1n.goodminton_shop_api.dtos.request.ProductVariantRequest;
 import com.lezh1n.goodminton_shop_api.dtos.response.ProductResponse;
+import com.lezh1n.goodminton_shop_api.dtos.response.ProductSpecificationResponse;
+import com.lezh1n.goodminton_shop_api.dtos.response.ProductVariantResponse;
 import com.lezh1n.goodminton_shop_api.services.ProductService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,6 +31,7 @@ public class ProductController {
 
     private final ProductService productService;
 
+    // Products
     @PostMapping
     public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductRequest request) {
         return ApiResponse.<ProductResponse>builder()
@@ -49,6 +57,62 @@ public class ProductController {
 
         return ApiResponse.<Page<ProductResponse>>builder()
                 .result(productPage)
+                .build();
+    }
+
+    @PutMapping("/{productId}")
+    public ApiResponse<ProductResponse> updateProduct(
+            @PathVariable @Min(value = 1, message = "Product ID must be greater than 0") Integer productId,
+            @Valid @RequestBody ProductRequest request) {
+        return ApiResponse.<ProductResponse>builder()
+                .result(productService.updateProduct(productId, request))
+                .build();
+    }
+
+    @DeleteMapping("/{productId}")
+    public ApiResponse<String> deleteProduct(
+            @PathVariable @Min(value = 1, message = "Product ID must be greater than 0") Integer productId) {
+        productService.deleteProduct(productId);
+        return ApiResponse.<String>builder()
+                .result("Xóa sản phẩm thành công")
+                .build();
+    }
+
+    // Specifications
+    @PostMapping("/{productId}/specifications")
+    public ApiResponse<ProductSpecificationResponse> addSpecificationToProduct(
+            @PathVariable @Min(value = 1, message = "Product ID must be greater than 0") Integer productId,
+            @Valid @RequestBody ProductSpecificationRequest request) {
+        return ApiResponse.<ProductSpecificationResponse>builder().build();
+    }
+
+    @DeleteMapping("/{productId}/specification/{specId}")
+    public ApiResponse<String> deleteSpecification(
+            @PathVariable @Min(value = 1, message = "Product ID must be greater than 0") Integer productId,
+            @PathVariable @Min(value = 1, message = "Specification ID must be greater than 0") Integer specId) {
+        productService.deleteSpecification(productId, specId);
+        return ApiResponse.<String>builder()
+                .result("Xóa thành công thông số sản phẩm")
+                .build();
+    }
+
+    // Variants
+    @PostMapping("{productId}/variants")
+    public ApiResponse<ProductVariantResponse> addVariantToProduct(
+            @PathVariable @Min(value = 1, message = "Product IDs must be greater than 0") Integer productId,
+            @Valid @RequestBody ProductVariantRequest request) {
+        return ApiResponse.<ProductVariantResponse>builder()
+                .result(productService.addVariantToProduct(productId, request))
+                .build();
+    }
+
+    @DeleteMapping("/{productId}/variants/{variantId}")
+    public ApiResponse<String> deleteVariant(
+            @PathVariable @Min(value = 1, message = "Product ID must be greater than 0") Integer productId,
+            @PathVariable @Min(value = 1, message = "Variant ID must be greater than 0") Integer variantId) {
+        productService.deleteVariant(productId, variantId);
+        return ApiResponse.<String>builder()
+                .result("Xóa thành công bản thể sản phẩm")
                 .build();
     }
 }
