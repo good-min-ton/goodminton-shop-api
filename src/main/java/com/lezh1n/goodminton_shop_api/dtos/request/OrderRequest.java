f03f0df82e38533ff90fc0810326477a8ec2bfec
Lezh1n <hoangphilong1208@gmail.com>
2025-08-05 20:01:56 +0700

    create order dtos

---
+++ src/main/java/com/lezh1n/goodminton_shop_api/dtos/request/OrderRequest.java
@@ -0,0 +1,16 @@
+package com.lezh1n.goodminton_shop_api.dtos.request;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class OrderRequest {
+    private Integer customerId;
+    private List<OrderItemRequest> items;
+    private String name;
+    private String phone;
+    private String address;
+    private String email;
+    private String note;
+}
