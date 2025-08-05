dbaa1850057e0da12562262accd7f4a30894f0d6
Lezh1n <hoangphilong1208@gmail.com>
2025-08-05 20:04:06 +0700

    refactor

--- src/main/java/com/lezh1n/goodminton_shop_api/repositories/OrderRepository.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/repositories/OrderRepository.java
@@ -1,11 +1,23 @@
 package com.lezh1n.goodminton_shop_api.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import com.lezh1n.goodminton_shop_api.entities.Order;
+import com.lezh1n.goodminton_shop_api.enums.OrderStatus;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    
+    @EntityGraph(attributePaths = {"customer", "orderItems.variantSize.variant.product"})
+    @NonNull
+    Optional<Order> findById(@NonNull Integer orderId);
 
+    @EntityGraph(attributePaths = {"customer", "orderItems.variantSize.variant.product"})
+    List<Order> findByOrderStatus(OrderStatus status);
 }
--- src/main/java/com/lezh1n/goodminton_shop_api/repositories/VariantSizeRepository.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/repositories/VariantSizeRepository.java
@@ -1,10 +1,13 @@
 package com.lezh1n.goodminton_shop_api.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import com.lezh1n.goodminton_shop_api.entities.VariantSize;
@@ -15,4 +18,14 @@ public interface VariantSizeRepository extends JpaRepository<VariantSize, Intege
     boolean existBySizeId(@Param("sizeId") Integer sizeId);
 
     List<VariantSize> findByVariantVariantId(Integer variantId);
+
+    @EntityGraph(attributePaths = {
+            "variant.product",
+            "variant.product.brand",
+            "variant.version",
+            "variant.color",
+            "size"
+    })
+    @NonNull
+    Optional<VariantSize> findById(@NonNull Integer variantSizeId);
 }
