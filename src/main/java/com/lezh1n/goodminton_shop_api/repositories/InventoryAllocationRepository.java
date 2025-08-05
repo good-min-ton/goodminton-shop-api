76022ba301acbfa99fc18f6e44036c82b4a10e54
Lezh1n <hoangphilong1208@gmail.com>
2025-08-05 20:03:19 +0700

    create allocation repository

---
+++ src/main/java/com/lezh1n/goodminton_shop_api/repositories/InventoryAllocationRepository.java
@@ -0,0 +1,11 @@
+package com.lezh1n.goodminton_shop_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lezh1n.goodminton_shop_api.entities.OrderItemInventoryAllocation;
+
+@Repository
+public interface InventoryAllocationRepository extends JpaRepository<OrderItemInventoryAllocation, Integer> {
+
+}
