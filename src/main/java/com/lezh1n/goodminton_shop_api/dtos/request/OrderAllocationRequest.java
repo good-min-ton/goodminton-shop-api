c53ec0aa551f8a779df54ee18a6582375c3afa53
Lezh1n <hoangphilong1208@gmail.com>
2025-08-08 15:34:13 +0700

    remove order ID

--- src/main/java/com/lezh1n/goodminton_shop_api/dtos/request/OrderAllocationRequest.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/dtos/request/OrderAllocationRequest.java
@@ -2,10 +2,11 @@ package com.lezh1n.goodminton_shop_api.dtos.request;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class OrderAllocationRequest {
-    private Integer orderId;
+    @NotEmpty(message = "ALLOCATION_LIST_EMPTY")
     private List<OrderItemAllocationRequest> allocations;
 }
