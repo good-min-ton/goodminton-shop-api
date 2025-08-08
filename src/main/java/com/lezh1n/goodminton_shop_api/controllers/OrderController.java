3d6f7ecb7272ecc9badf572b64cc4a26e4ef7d91
Lezh1n <hoangphilong1208@gmail.com>
2025-08-08 10:05:11 +0700

    merge commit

---
+++ src/main/java/com/lezh1n/goodminton_shop_api/controllers/OrderController.java
@@ -0,0 +1,13 @@
+package com.lezh1n.goodminton_shop_api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+}
