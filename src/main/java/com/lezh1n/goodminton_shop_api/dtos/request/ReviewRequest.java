b3a7dcaf3b654cf75cae8034aa86f55eb47a65e4
Lezh1n <hoangphilong1208@gmail.com>
2025-08-14 19:48:55 +0700

    refactor dto

--- src/main/java/com/lezh1n/goodminton_shop_api/dtos/request/ReviewRequest.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/dtos/request/ReviewRequest.java
@@ -2,14 +2,14 @@ package com.lezh1n.goodminton_shop_api.dtos.request;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ReviewRequest {
     @Min(value = 1, message = "REVIEW_RATING_OUT_OF_RANGE")
     @Max(value = 5, message = "REVIEW_RATING_OUT_OF_RANGE")
-    @NotBlank(message = "REVIEW_RATING_BLANK")
+    @NotNull(message = "REVIEW_RATING_BLANK")
     private Integer rating;
     private String comment;
 }
