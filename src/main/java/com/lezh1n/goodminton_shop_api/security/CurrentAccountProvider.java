cd2b06a2588356585127e295afe040fe580b57a6
Lezh1n <hoangphilong1208@gmail.com>
2025-08-14 17:15:44 +0700

    create account provider component

---
+++ src/main/java/com/lezh1n/goodminton_shop_api/security/CurrentAccountProvider.java
@@ -0,0 +1,26 @@
+package com.lezh1n.goodminton_shop_api.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.lezh1n.goodminton_shop_api.entities.Account;
+import com.lezh1n.goodminton_shop_api.exceptions.AppException;
+import com.lezh1n.goodminton_shop_api.exceptions.ErrorCode;
+
+@Component
+public class CurrentAccountProvider {
+    public Account getCurrentAccount() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AppException(ErrorCode.AUTH_UNAUTHENTICATED);
+        }
+
+        return (Account) authentication.getPrincipal();
+    }
+
+    public Integer getCurrentAccountId() {
+        return getCurrentAccount().getAccountId();
+    }
+}
