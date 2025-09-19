afa4393110d7950920eab2a02cb19230d486f999
Lezh1n <hoangphilong1208@gmail.com>
2025-09-19 10:34:42 +0700

    fix case sensitive in request param

---
+++ src/main/java/com/lezh1n/goodminton_shop_api/configurations/WebConfig.java
@@ -0,0 +1,22 @@
+package com.lezh1n.goodminton_shop_api.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.lezh1n.goodminton_shop_api.enums.UserRole;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new Converter<String, UserRole>() {
+            @Override
+            public UserRole convert(@NonNull String source) {
+                return UserRole.valueOf(source.trim().toUpperCase());
+            }
+        });
+    }
+}
