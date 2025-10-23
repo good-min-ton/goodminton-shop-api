0df8aa7b54edfdda3ab8387e6b51ced226951091
Lezh1n <hoangphilong1208@gmail.com>
2025-10-23 21:05:09 +0700

    refactor: update global converter for all enums

--- src/main/java/com/lezh1n/goodminton_shop_api/configurations/WebConfig.java
+++ src/main/java/com/lezh1n/goodminton_shop_api/configurations/WebConfig.java
@@ -2,29 +2,44 @@ package com.lezh1n.goodminton_shop_api.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.convert.converter.ConverterFactory;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.lezh1n.goodminton_shop_api.enums.AccountStatus;
-import com.lezh1n.goodminton_shop_api.enums.UserRole;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new Converter<String, UserRole>() {
-            @Override
-            public UserRole convert(@NonNull String source) {
-                return UserRole.valueOf(source.trim().toUpperCase());
-            }
-        });
+    public void addFormatters(@NonNull FormatterRegistry registry) {
+        registry.addConverterFactory(new StringToEnumIgnoreCaseConverterFactory());
+    }
 
-        registry.addConverter(new Converter<String, AccountStatus>() {
+    @SuppressWarnings("rawtypes")
+    private static class StringToEnumIgnoreCaseConverterFactory implements ConverterFactory<String, Enum> {
+
+        @Override
+        @NonNull
+        public <T extends Enum> Converter<String, T> getConverter(@NonNull Class<T> targetType) {
+            return new StringToEnumConverter<>(targetType);
+        }
+
+        @RequiredArgsConstructor
+        private static class StringToEnumConverter<T extends Enum<T>> implements Converter<String, T> {
+            private final Class<T> enumType;
+
             @Override
-            public AccountStatus convert(@NonNull String source) {
-                return AccountStatus.valueOf(source.trim().toUpperCase());
+            @Nullable
+            public T convert(@NonNull String source) {
+                if (source.trim().isEmpty()) {
+                    return null;
+                }
+                return Enum.valueOf(enumType, source.trim().toUpperCase());
             }
-        });
+
+        }
+
     }
 }
